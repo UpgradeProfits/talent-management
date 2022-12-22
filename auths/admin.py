20e2b36581cc2430716cd89cd815a7069c29a6f6
DilosiRichfield <richfield_dilosi@upgradeprofits.com>
2022-12-22 15:00:13 -0800

    backend for the basic sign-up

---
+++ auths/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from .models import User
+
+admin.site.site_header = 'UpgradeProfits~~test Admin'
+admin.site.index_title = 'Admin'
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin', 'first_name', 'last_name', 'tel',)
+    list_filter = ('admin', 'TC')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name', 'tel', 'nationality')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin', 'TC')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'tel', 'nationality')}
+         ),
+    )
+    search_fields = ('email', 'first_name', 'last_name', 'tel', 'nationality')
+    ordering = ('email', 'first_name', 'last_name', 'tel', 'nationality')
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+# Remove Group Model from admin. We're not using it.
+# admin.site.unregister(Group)
+
+
+# admin.site.register(UserBio)
---
+++ auths/models.py
@@ -0,0 +1,156 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+from datetime import datetime, timedelta
+from django.core.exceptions import ValidationError
+from django.conf import settings
+from django.utils.timezone import now
+
+
+# Overriding the default django authentication
+"""
+    this is for the basic sign-up for Appointment setters and Closers :)
+"""
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, tel, nationality, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('You must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name= first_name,
+            last_name= last_name,
+            tel= tel,
+            nationality= nationality,
+            password=password
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, first_name, last_name, tel, nationality, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name= first_name,
+            last_name= last_name,
+            tel= tel,
+            nationality= nationality,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, tel, nationality, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name= first_name,
+            last_name= last_name,
+            tel= tel,
+            nationality=nationality,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    objects = UserManager()
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True,)
+    first_name= models.CharField(default='', null=False, blank=False, max_length=30)
+    last_name= models.CharField(default='', null=False, blank=False, max_length=30)
+    tel = models.CharField(default='', null=False, blank=False, max_length=30)
+    nationality= models.CharField(default="", null=False, blank=False, max_length=50)
+    active = models.BooleanField(default=True)
+    TC = models.BooleanField(default=False)#Terms and Conditions
+    staff = models.BooleanField(default=False)  # a admin user; non super-user
+    admin = models.BooleanField(default=False)  # a superuser
+    # notice the absence of a "Password field", that is built in.
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'tel', 'nationality']  # Email & Password are required by default.
+
+    def get_full_name(self):
+        # The user is identified by their first&last name
+        fullname = '%s %s' % (self.first_name, self.last_name)
+        return fullname
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.first_name
+
+    def __str__(self):  # __unicode__ on Python 2
+        fullname = '%s %s' % (self.first_name, self.last_name)
+        return fullname
+
+    def has_perms(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def has_agreed(self):
+        """Is it a user?"""
+        return self.TC
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        return self.staff
+
+    @property
+    def is_admin(self):
+        """Is the user a admin member?"""
+        return self.admin
+
+    @property
+    def is_active(self):
+        """Is the user active?"""
+        return self.active
+
+
+# class Appointment_holders(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
+#     code = models.CharField(default='', blank=True, max_length=9)
+#     qrcode = models.ImageField(upload_to='user_QRC_auth/', blank=True)
+#     created = models.DateTimeField(auto_now_add=True, editable=True)
+
+#     def __str__(self):
+#         concatenate = '%s%s%s' % (self.user, '-', self.code)
+#         return str(concatenate)
+
+#     def save(self, *args, **kwargs):
+#         qrcode_image = qrcode.make(f"Name: {self.user}\nAuthor: {self.code}")
+#         canvas = Image.new('RGB', (430, 430), 'white')
+#         draw = ImageDraw.Draw(canvas)
+#         canvas.paste(qrcode_image)
+#         fname = f"{'%s%s%s' % (self.user, '-', self.code)}.png"
+#         buffer = BytesIO()
+#         canvas.save(buffer, 'PNG')
+#         self.qrcode.save(fname, File(buffer), save=False)
+#         canvas.close()
+#         super().save(*args, **kwargs)
+
