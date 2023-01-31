7d313c7cbd93774b99aa314ce48cdd9fac21fa92
khan8484 <hasnainsaleem@upgradeprofits.com>
2023-02-01 01:42:17 +0500

    BD CHANGES

--- core/settings.py
+++ core/settings.py
@@ -88,13 +88,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'TMS',
+            'USER': 'TMS_admin',
+            'PASSWORD': '1234',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
