9e269d1972411f7ef7457720abeb7ee2d4c1640f
khan8484 <hasnainsaleem@upgradeprofits.com>
2023-02-02 22:51:31 +0500

    whitenoise changes

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['.highticketrecruit.com','.www.highticketrecruit.com','138.197.
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
