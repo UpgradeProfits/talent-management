4ab84fe1bfe0e9e2fb1cfe72c51d7e075ebe2924
khan8484 <hasnainsaleem@upgradeprofits.com>
2023-02-18 06:58:15 +0500

    new pull for update logo

---
+++ collected_static/js/userprofile/main1.js
@@ -0,0 +1,36 @@
+(function($) {
+
+	"use strict";
+
+
+	$(".js-select2").select2({
+		width: "500px",
+		margin:"5px",
+		closeOnSelect : false,
+		placeholder : "Click to select an option",
+		allowHtml: true,
+		allowClear: true,
+		tags: true // создает новые опции на лету
+		});
+
+	$('.icons_select2').select2({
+		width: "500px",
+		margin:"5px",
+		templateSelection: iformat,
+		templateResult: iformat,
+		allowHtml: true,
+		placeholder: "Click to select an option",
+		dropdownParent: $( '.select-icon' ),//обавили класс
+		allowClear: true,
+		multiple: false
+	});
+
+
+	function iformat(icon, badge,) {
+		var originalOption = icon.element;
+		var originalOptionBadge = $(originalOption).data('badge');
+	 
+		return $('<span><i class="fa ' + $(originalOption).data('icon') + '"></i> ' + icon.text + '<span class="badge">' + originalOptionBadge + '</span></span>');
+	}
+
+})(jQuery);
